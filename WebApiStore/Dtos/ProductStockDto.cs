75d498d79b3d3d7604ceac005b8b771a3e410597
Jose Carlos HR <josecarlos@srlpunto.com>
2023-02-16 22:08:25 -0400

    add Dto product whit Stock

---
+++ WebApiStore/Dtos/ProductStockDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace WebApiStore.Dtos
+{
+    public class ProductStockDto : ProductDto
+    {
+        public int Stock { get; set; }
+
+    }
+}
